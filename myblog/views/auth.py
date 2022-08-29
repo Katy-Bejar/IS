e43d1ec6bb7a5d1c48a0ce8773318fac2ef23233
Yanira1612 <83191641+Yanira1612@users.noreply.github.com>
2022-08-28 23:16:09 -0500

    Update auth.py

--- myblog/views/auth.py
+++ myblog/views/auth.py
@@ -2,9 +2,7 @@
 #Importaciones necesarias
 import functools
 from os import error
-from flask import(
-    render_template, Blueprint, flash, g, redirect, request, session, url_for
-)
+from flask import(render_template, Blueprint, flash, g, redirect, request, session, url_for)
 from werkzeug.security import check_password_hash, generate_password_has
 from myblog.models.user import Usuario
 from myblog import db
